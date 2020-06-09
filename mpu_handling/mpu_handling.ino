c2f780bb8129b037cb7f44c7b307606102e9ebeb
dominc8 <kormandominik@gmail.com>
2020-06-09 18:44:06 +0200

    Added sampling with constant frequency 1kHz and computation of offset

--- mpu_handling/mpu_handling.ino
+++ mpu_handling/mpu_handling.ino
@@ -16,7 +16,7 @@
 #define MPU6050_WHO_AM_I        0x75
 
 #define MPU6050_ACCEL_BURST_READ_START  MPU6050_ACCEL_XOUT_H
-#define MPU6050_ACCEL_BURST_READ_SIZE   (MPU6050_ACCEL_ZOUT_L - MPU6050_ACCEL_XOUT_H + 1)
+#define MPU6050_ACCEL_BURST_READ_SIZE   (MPU6050_ACCEL_YOUT_L - MPU6050_ACCEL_XOUT_H + 1)
 
 
 struct ConfigMessage
@@ -30,9 +30,11 @@ const ConfigMessage config_sequence[] =
     { MPU6050_PWR_MGMT_1,   0x01 },
     { MPU6050_CONFIG,       0x00 },
     { MPU6050_GYRO_CONFIG,  0x00 },
-    { MPU6050_ACCEL_CONFIG, 0x00 },
+    { MPU6050_ACCEL_CONFIG, 0x08 }, /* 2b, 2g - 16g, 0x18 -> 16g*/
 };
 
+
+volatile uint8_t timer_flag = 0;
 
 void setup()
 {
@@ -57,14 +59,45 @@ void setup()
     {
         Serial.println("Error! Wrong device address!");
     }
+
+    /* Configure 1kHz timer */
+    noInterrupts();
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1  = 0;
+    OCR1A = 249;
+    TCCR1A |= (1 << WGM12);
+    TCCR1B |= (3 << CS10);
+    TIMSK1 |= (1 << OCIE1A);
+    interrupts();
+
 }
 
 void loop()
 {
-    uint8_t buffer[6];
-    if (MPU6050_ACCEL_BURST_READ_SIZE == I2Cdev::readBytes(MPU6050_ADDR, MPU6050_ACCEL_BURST_READ_START, MPU6050_ACCEL_BURST_READ_SIZE, buffer))
+    static uint8_t buffer[MPU6050_ACCEL_BURST_READ_SIZE];
+    static int32_t x_vel = 0, y_vel = 0;
+    static int16_t x_dvel = 0, y_dvel = 0;
+    static int32_t out_data[2];
+    if (timer_flag == 1)
     {
-        /* Send data through bluetooth */
-        
+        timer_flag = 0;
+        if (MPU6050_ACCEL_BURST_READ_SIZE == I2Cdev::readBytes(MPU6050_ADDR, MPU6050_ACCEL_BURST_READ_START, MPU6050_ACCEL_BURST_READ_SIZE, buffer))
+        {
+            x_dvel = (int16_t)((((uint16_t)buffer[0]) << 8) + buffer[1]);
+            y_dvel = (int16_t)((((uint16_t)buffer[2]) << 8) + buffer[3]);
+            out_data[0] = x_vel + x_dvel/2;
+            out_data[1] = y_vel + y_dvel/2;
+            x_vel += x_dvel;
+            y_vel += y_dvel;
+            /* Send data through bluetooth */
+
+        }
     }
+}
+
+
+ISR(TIMER1_COMPA_vect)
+{
+    timer_flag = 1;
 }
